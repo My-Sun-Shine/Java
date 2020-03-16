eb79d6510ab180989798df98b9cb5991b20e89b3
My-Sun-Shine <1399237176@qq.com>
2020-03-16 15:33:49 +0800

    转账项目控制器
    Signed-off-by: My-Sun-Shine <1399237176@qq.com>

---
+++ WebLearn2.0/src/com/Controller/TAccountController.java
@@ -0,0 +1,41 @@
+package com.Controller;
+
+import com.Dao.Impl.TAccountDaoImpl;
+import com.Dao.TAccountDao;
+import com.Service.Impl.TAccountServiceImpl;
+import com.Service.TAccountService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @Classname TAccountController
+ * @Date 2020/3/15 21:54
+ * @Created by Falling Stars
+ * @Description 转账项目控制器
+ */
+@WebServlet(urlPatterns = "/com/Controller/TAccount")
+public class TAccountController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //super.doGet(req, resp);
+        System.out.println("开始执行转账操作");
+
+        //接收表单参数
+        String outAccount = request.getParameter("outAccount");//转出账号
+        String intoAccount = request.getParameter("intoAccount");//转入账号
+        String BalanceStr = request.getParameter("Balance");//金额
+
+        TAccountService accountService = new TAccountServiceImpl();
+
+        boolean result = accountService.tAccount(outAccount, intoAccount, BalanceStr);
+        PrintWriter writer = response.getWriter();
+        writer.print(result);
+        writer.close();
+    }
+}
