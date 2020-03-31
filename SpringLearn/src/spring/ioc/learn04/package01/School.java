ddf0f94c7d08efaeb3d75ec94c4156ac4864038b
My-Sun-Shine <1399237176@qq.com>
2020-03-31 23:24:12 +0800

    Spring学习005：引用类型属性 byName自动注入
    Signed-off-by: My-Sun-Shine <1399237176@qq.com>

---
+++ SpringLearn/src/spring/ioc/learn04/package01/School.java
@@ -0,0 +1,37 @@
+package spring.ioc.learn04.package01;
+
+/**
+ * @Classname School
+ * @Date 2020/3/31 20:03
+ * @Created by Falling Stars
+ * @Description
+ */
+public class School {
+    private String name;
+    private String address;
+
+
+    public School() {
+        super();
+        System.out.println("School的无参数构造方法");
+    }
+
+    public School(String name, String address) {
+        System.out.println("School的有参数构造方法");
+        this.name = name;
+        this.address = address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "School [name=" + name + ", address=" + address + "]";
+    }
+}
---
+++ SpringLearn/src/spring/ioc/learn04/package01/Student.java
@@ -0,0 +1,54 @@
+package spring.ioc.learn04.package01;
+
+import spring.ioc.learn04.package01.School;
+
+/**
+ * @Classname Student
+ * @Date 2020/3/31 19:27
+ * @Created by Falling Stars
+ * @Description
+ */
+public class Student {
+    private String name;
+    private int age;
+
+    //定义引用类型
+    private School mySchool;
+
+
+    public Student() {
+        super();
+        System.out.println("student的无参数构造方法");
+    }
+
+    public Student(String name, int age, School school) {
+        System.out.println("student的有参数构造方法");
+        this.name = name;
+        this.age = age;
+        this.mySchool = school;
+    }
+
+    public void setName(String name) {
+        System.out.println("setName:" + name);
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        System.out.println("setAge:" + age);
+        this.age = age;
+    }
+
+    public void setMySchool(School school) {
+        System.out.println("setSchool:" + school);
+        this.mySchool = school;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", school=" + mySchool +
+                '}';
+    }
+}
