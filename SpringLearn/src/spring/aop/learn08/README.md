c3a5857f5c4a77204b25b1d6b91f5d3a2b59b9da
My-Sun-Shine <1399237176@qq.com>
2020-04-06 22:45:29 +0800

    Spring学习018：配置文件设置使用cglib动态代理
    Signed-off-by: My-Sun-Shine <1399237176@qq.com>

---
+++ SpringLearn/src/spring/aop/learn08/README.md
@@ -0,0 +1,5 @@
+### 配置文件设置使用cglib动态代理
+```
+<!--proxy-target-class="true",使用cglib动态代理-->
+<aop:aspectj-autoproxy proxy-target-class="true"/>
+```
---
+++ SpringLearn/src/spring/aop/learn08/SomeServiceImpl.java
@@ -0,0 +1,14 @@
+package spring.aop.learn08;
+
+/**
+ * @Classname SomeServiceImpl
+ * @Date 2020/4/4 13:29
+ * @Created by Falling Stars
+ * @Description
+ */
+public class SomeServiceImpl implements SomeService {
+    @Override
+    public void doThird() {
+        System.out.println("====业务方法doThird===");
+    }
+}
