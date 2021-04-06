55c519e0b9cb40e6e4e42bd340839b77dfd9517d
My-Sun-Shine <1399237176@qq.com>
2021-04-06 23:36:57 +0800

    SpringBootLearn 0018：提取公共页，正删改查
    Signed-off-by: My-Sun-Shine <1399237176@qq.com>

---
+++ SpringBootWeb/src/main/java/com/springbootweb/controller/EmployeeController.java
@@ -0,0 +1,91 @@
+package com.springbootweb.controller;
+
+import com.springbootweb.dao.DepartmentDao;
+import com.springbootweb.dao.EmployeeDao;
+import com.springbootweb.entity.Department;
+import com.springbootweb.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+/**
+ * @Classname EmployeeController
+ * @Date 2021/4/6 22:14
+ * @Created by FallingStars
+ * @Description
+ */
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    @Autowired
+    private DepartmentDao departmentDao;
+
+    //查询所有员工返回列表页面
+    @GetMapping("/list")
+    public String list(Model model) {
+        Collection<Employee> employees = employeeDao.getAll();
+        //放在请求域中
+        model.addAttribute("employee", employees);
+        // thymeleaf默认就会拼串
+        // classpath:/templates/xxxx.html
+        return "emp/list";
+    }
+
+    //来到员工添加页面
+    @GetMapping("/toAddPage")
+    public String toAddPage(Model model) {
+        //来到添加页面,查出所有的部门，在页面显示
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departs", departments);
+        return "emp/add";
+    }
+
+    //来到修改页面，查出当前员工，在页面回显
+    @GetMapping("/toEditPage/{id}")
+    public String toEditPage(@PathVariable("id") Integer id, Model model) {
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("emp", employee);
+
+        //页面要显示所有的部门列表
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departs", departments);
+        //回到修改页面(add是一个修改添加二合一的页面);
+        return "emp/add";
+    }
+
+    //员工添加
+    //SpringMVC自动将请求参数和入参对象的属性进行一一绑定；要求请求参数的名字和javaBean入参的对象里面的属性名是一样的
+    @PostMapping("/AddOrUpdate")
+    public String addEmployee(Employee employee) {
+        //来到员工列表页面
+
+        System.out.println("保存的员工信息：" + employee);
+        //保存员工
+        employeeDao.save(employee);
+        // redirect: 表示重定向到一个地址  /代表当前项目路径
+        // forward: 表示转发到一个地址
+        return "redirect:/employee/list";
+    }
+
+    //员工修改；需要提交员工id；
+    @PutMapping("/AddOrUpdate")
+    public String updateEmployee(Employee employee) {
+        System.out.println("修改的员工数据：" + employee);
+        employeeDao.save(employee);
+        return "redirect:/employee/list";
+    }
+
+    //员工删除
+    @DeleteMapping("/deleteEmployee/{id}")
+    public String deleteEmployee(@PathVariable("id") Integer id) {
+        employeeDao.delete(id);
+        return "redirect:/employee/list";
+    }
+
+}
