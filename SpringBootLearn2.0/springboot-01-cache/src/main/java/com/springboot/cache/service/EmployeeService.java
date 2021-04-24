5d7623aa4d615bd487f15c18dbb066ea6360ccc5
My-Sun-Shine <1399237176@qq.com>
2021-04-24 18:46:51 +0800

    SpringBootLearn 0028：cache学习：@CachePut、@CacheEvict、@Caching、@CacheConfig的使用
    Signed-off-by: My-Sun-Shine <1399237176@qq.com>

--- SpringBootLearn2.0/springboot-01-cache/src/main/java/com/springboot/cache/service/EmployeeService.java
+++ SpringBootLearn2.0/springboot-01-cache/src/main/java/com/springboot/cache/service/EmployeeService.java
@@ -3,7 +3,7 @@ package com.springboot.cache.service;
 import com.springboot.cache.bean.Employee;
 import com.springboot.cache.mapper.EmployeeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.*;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,6 +13,8 @@ import org.springframework.stereotype.Service;
  * @Description
  */
 @Service
+// 抽取缓存的公共配置
+@CacheConfig(cacheNames = "employee")
 public class EmployeeService {
     @Autowired
     EmployeeMapper employeeMapper;
@@ -65,17 +67,57 @@ public class EmployeeService {
         return emp;
     }
 
+    /**
+     * 注解@CachePut：既调用方法，修改了数据库的某个数据，同时更新缓存
+     * 运行时机：1、先调用目标方法   2、将目标方法的结果缓存起来
+     * 注解@Cacheable的key是不能用#result：@Cacheable是在方法运行之前执行，result为null
+     * 测试步骤：
+     * 1、查询1号员工；查到的结果会放在缓存中
+     * 2、再次查询还是之前的结果
+     * 3、更新1号员工，将方法的返回值也放进缓存了
+     * 4、查询1号员工，是更新后的员工
+     * <p>
+     * key = "#employee.id":使用传入的参数的员工id；
+     * key = "#result.id"：使用返回后的id
+     * 不指定key的时候，key是传入的employee对象，值是返回的employee对象；
+     */
+    @CachePut(value = {"employee"}, key = "#result.id")
     public Employee updateEmp(Employee employee) {
         System.out.println("updateEmp:" + employee);
         employeeMapper.updateEmp(employee);
         return employee;
     }
 
+    /**
+     * 注解@CacheEvict：缓存清除
+     * <p>
+     * key：指定要清除的数据
+     * allEntries = true：指定清除这个缓存中所有的数据
+     * <p>
+     * 缓存的清除是否在方法之前执行
+     * beforeInvocation = false：默认代表缓存清除操作是在方法执行之后执行，如果出现异常缓存就不会清除
+     * beforeInvocation = true：代表清除缓存操作是在方法运行之前执行，无论方法是否出现异常，缓存都清除
+     */
+    @CacheEvict(value = {"employee"}, key = "#id")
     public void deleteEmp(Integer id) {
         System.out.println("deleteEmp:" + id);
         employeeMapper.deleteEmpById(id);
+        //int i = 10/0;
     }
 
+    /**
+     * 定义复杂的缓存规则
+     * 因为有注解@CachePut，所以每次都会执行方法
+     */
+    @Caching(
+            cacheable = {
+                    @Cacheable(/*value="employee",*/key = "#lastName")
+            },
+            put = {
+                    @CachePut(/*value="employee",*/key = "#result.id"),
+                    @CachePut(/*value="employee",*/key = "#result.email")
+            }
+    )
     public Employee getEmpByLastName(String lastName) {
         return employeeMapper.getEmpByLastName(lastName);
     }
